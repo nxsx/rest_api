0788a906a3a767d2f9af32010f85ec66e4749f24
Thanat Dampawanwong <thanat.damp@gmail.com>
2020-08-27 14:04:07 +0700

    update echo text in index.php on line 10 from work branch

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $db = new Database("localhost", "registration_pdo", "root", "");
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     echo json_encode($db->query("SELECT * FROM tbl_user"));
 } else if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    echo "This is post metho.d";
+    echo "This is post method.";
 } else {
     http_response_code(405);
 }
