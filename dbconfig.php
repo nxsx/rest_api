dd5fa70be3300c76b4dfc9b1c99b4f0b4588eb7f
Thanat Dampawanwong <thanat.damp@gmail.com>
2020-08-27 13:18:14 +0700

    update value of con in dbconfig.php on line 8 from work branch

--- dbconfig.php
+++ dbconfig.php
@@ -5,7 +5,7 @@
 
         public function __construct($host, $dbname, $username, $password)
         {
-            $con = new PDO("mysql:host".$host.";dbname=".$dbname.";", $username, $password);
+            $con = new PDO("mysql:host=".$host.";dbname=".$dbname.";", $username, $password);
             $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->pdo = $con;
         }
