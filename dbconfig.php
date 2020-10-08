24f6639bdcf2f8d7a4ba16a90f63e47679f7f4ca
Thanat Dampawanwong <thanat.damp@gmail.com>
2020-10-08 14:03:34 +0700

    change to used const for connection string

--- dbconfig.php
+++ dbconfig.php
@@ -1,24 +1,37 @@
 <?php
 
-    class Database {
-        private $pdo;
+    class DATABASE {
 
-        public function __construct($host, $dbname, $username, $password)
+        private $mysql;
+        private $con;
+
+        private const MYSQL_CONNECTION = "mysql:host=localhost;dbname=registration_pdo;";
+        private const MYSQL_USERNAME = "root";
+        private const MYSQL_PASSWORD = "nopassword";
+
+        public function __construct()
         {
-            $con = new PDO("mysql:host=".$host.";dbname=".$dbname.";", $username, $password);
-            $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $this->pdo = $con;
+            $this->con = new PDO(
+                DATABASE::MYSQL_CONNECTION,
+                DATABASE::MYSQL_USERNAME,
+                DATABASE::MYSQL_PASSWORD
+            );
+            $this->con->setAttribute(
+                PDO::ATTR_ERRMODE,
+                PDO::ERRMODE_EXCEPTION
+            );
+            $this->mysql = $this->con;
         }
 
         public function query($show) {
-            $stmt = $this->pdo->prepare($show);
+            $stmt = $this->mysql->prepare($show);
             $stmt->execute();
-
             if ($show) {
-                $data = $stmt->fetchAll();
+                $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 return $data;
             }
         }
+
     }
 
 ?>
